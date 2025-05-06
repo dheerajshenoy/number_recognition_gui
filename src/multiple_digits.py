b387bdab7081cc5f5f3dd6cb5dcd4abe0a2fea5b
Dheeraj Shenoy <dheerajshenoy22@gmail.com>
2025-05-06 12:02:10 +0530

    Add requirements file

---
+++ src/multiple_digits.py
@@ -0,0 +1,12 @@
+import torch
+import matplotlib
+matplotlib.use('Qt6Agg')
+from torch import nn
+from torch.utils.data import DataLoader
+import cv2
+import matplotlib.pyplot as plt
+
+image = cv2.imread("./2310545_312579-P8HU6K-966.jpg")
+
+plt.imshow(image)
+plt.show()
